4482b0f5f9d454a2c4b1e93d94f7314f171811fe
Tarun Sharma <tarunshama2002@gmail.com>
2026-03-10 17:01:56 +0530

    first

--- Basics/index.js
+++ Basics/index.js
@@ -1,16 +1,22 @@
+// Building a Node.js HTTP server using Express. --> Node.js HTTP Server
+
 require("dotenv").config(); // to use .env file // load variables from .env file
-console.log("Tarun Sharma")
 
 
-const express = require('express') //  we have used "require" means we require Express. We can also use another syntex
-const app = express()
+import express from "express"
+// We can also use another syntex --> const express = require('express')
+
+const app = express() // express() creates an Express app --> This app will act as your server
+
 const port = 3000
+app.listen(3000)   // It means the server will run on port 3000 and listen for incoming requests from the client (browser).
+// We open the server in the browser using http://localhost:3000
 
-app.get('/', (req, res) => { // get is a request . here made on the home page
+app.get('/', (req, res) => { // get is a request . here made on the home route by client (browser) --> using http://localhost:3000/
   res.send('Hello World!')
 })
- 9
-app.get('/twitter',(req,res) =>{
+
+app.get('/twitter',(req,res) =>{  // get is a request . here made on the twitter route
     res.send('twitter is opened')
 })
 app.get ('/login', (req,res) =>{
